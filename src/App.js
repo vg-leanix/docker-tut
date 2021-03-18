bdb00e4455bbb1cbda2744e4e78074bf0a8d1eae
Vince <vincent.groves@leanix.net>
2021-03-18 12:37:57 +0100

    new build

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Test with Yannick</p>
+        <p>CSM Team Meeting</p>
         <a
           className="App-link"
           href="https://reactjs.org"
