5cff6fb07305bdcd269f287c0be934124cf4f845
Vince <vincent.groves@leanix.net>
2021-07-27 17:28:20 +0200

    test 1728

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>CSM Team Meeting</p>
+        <p>CSM Team Showcase 1727</p>
         <a
           className="App-link"
           href="https://reactjs.org"
